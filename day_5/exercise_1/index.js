3dadae7a7d6d4f8248edeb3a493e092cc2bb055a
Quan Phan <hoangquanphan2906@gmail.com>
2020-08-04 11:40:08 +0700

    Add day 5 exercises

---
+++ day_5/exercise_1/index.js
@@ -0,0 +1,11 @@
+// Hoàn thiện code sau để thỏa yêu cầu:
+// Khi người dùng bấm vào button thì số ngày tăng lên 1
+
+// increBut = document.getElementById("increment-button");
+// dayNum =
+
+// function handleIncrease() {
+
+// }
+
+// increBut.addEventListener("click", handleIncrease)
---
+++ day_5/exercise_2/index.js
@@ -0,0 +1,16 @@
+// 1. Hãy import file JS vào HTML
+
+// 2. Mỗi khi người dùng rê chuột lên chữ ProgrammingHub, chữ ấy sẽ đổi thành
+// màu đỏ
+
+// title = document.getElementById("title");
+// title.addEventListener("mouseover", function (e) {
+
+// });
+
+// 3. Khi người dùng bấm button thì các thông tin cá nhân của bạn sẽ hiện ra
+//  và button sẽ chuyển màu đỏ và chữ trong button chuyển màu xanh dương
+// infoButton = document.getElementById("see-info");
+// infoButton.addEventListener("click", function (e) {
+//     e.target.style.backgroundColor = "red";
+// });
