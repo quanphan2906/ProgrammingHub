6503a22e8149e713da9d49a5bc3a979ad42d304f
Quan Phan <hoangquanphan2906@gmail.com>
2020-08-17 11:37:53 +0700

    Add exercise

---
+++ day_5/exercise_3/index.js
@@ -0,0 +1,17 @@
+// 1. Bắt sự kiện người dùng submit form. Xác nhận những điều sau
+
+// 2. Kiểm tra email người dùng có phải là gmail/yahoo không
+// Nếu không phải --> yêu cầu nhập mail gmail/yahoo
+
+// 3. Kiểm tra password người dùng có đủ 8 ký tự, trong đó có 1 ký tự viết hoa
+// Nếu không, yêu cầu người dùng nhập lại theo yêu cầu trên
+
+// 4. Kiểm tra người đủ trên 13 tuổi không.
+// Nếu không, người dùng không được quyền dùng app
+
+// 5.Kiểm tra xem người dùng có điền tên anh hùng họ yêu thích không
+// Nếu không, yêu cầu họ điền
+
+// Nếu thỏa hết các điều trên --> in thông tin người dùng ra console
+
+// Để làm được những điều trên, có thể gán thêm id/class bên html
